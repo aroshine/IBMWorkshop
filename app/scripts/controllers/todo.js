c4699f765776e3cb5fc849bc8bf5ed2bf4a8066d
aroshine <aroshine@kth.se>
2017-01-26 23:34:17 +0100

    app.js and scripts

---
+++ app/scripts/controllers/todo.js
@@ -0,0 +1,26 @@
+'use strict';
+var angular = require('angular');
+angular.module('todoListApp')
+.controller('todoCtrl', function($scope, dataService) {
+  $scope.saveTodos = function() {
+    var filteredTodos = $scope.todos.filter(function(todo){
+      if(todo.edited) {
+        return todo
+      };
+    })
+    dataService.saveTodos(filteredTodos)
+    .finally($scope.resetTodoState());
+  }; 
+  $scope.resetTodoState = function() {
+    $scope.todos.forEach(function(todo){
+      todo.edited = false;
+
+    });
+  };
+  $scope.deleteTodo = function (todo, index) {
+    dataService.deleteTodo(todo).then(function () {
+        $scope.todos.splice(index, 1);
+    });
+  };
+
+});

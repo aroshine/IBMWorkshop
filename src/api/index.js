995a80dad38325d83ec66e048ba86eec8ab68d29
aroshine <aroshine@kth.se>
2017-01-26 23:32:59 +0100

    src code

---
+++ src/api/index.js
@@ -0,0 +1,56 @@
+//acts as our API module
+'use strict';
+
+var express = require('express');
+var router = express.Router(); //using a router to skip writing /api/ in every route
+var Todo = require('../models/todo');
+//var todos = require('../../mock/todos.json'); 
+
+//creating first route for our API
+router.get('/todos', function(req, res){
+	Todo.find({}, function(err, todos){
+		if(err){
+			//do something
+			return res.status(500).json({message: err.message});
+		}
+		res.json({todos:todos});
+	});
+});
+// TODO: Add POST route to create new entries
+router.post('/todos', function(req, res){
+	var todo = req.body;
+	Todo.create(todo, function(err, todo){
+		if(err){
+			return res.status(500).json({message: err.message});
+		}
+		res.json({'todo': todo, message: 'Todo Created'});
+	});
+});
+// TODO: Add PUT route to update existing entries
+router.put('/todos/:id', function(req, res){
+	var id = req.params.id;
+	var todo = req.body;
+	if(todo && todo._id !== id){
+		return res.status(500).json({err: "Ids don't match!"});
+
+	}
+	Todo.findByIdAndUpdate(id, todo, {new:true}, function(err, todo){
+		if(err){
+			return res.status(500).json({message: err.message});
+		}
+		res.json({'todo': todo, message: 'Todo Updated'});
+	});
+});
+// TODO: Add DELETE route to delete entries
+router.delete('/todos/:id', function (req, res) {
+    var todoId = req.params.id; // This maps to the :id in the url
+    Todo.findByIdAndRemove(todoId, function (err, result) {
+        if (err) {
+            res.status(500).json({ message: err.message });
+        } else {
+            res.json({ message: 'Deleted Todo' });
+        }
+    });
+});
+
+module.exports = router;
